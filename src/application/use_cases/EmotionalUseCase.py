b1176fb08f3fa2d19ea6497f94ece3cf3cd81704
prince-de-len <144515902+prince-de-len@users.noreply.github.com>
2025-11-16 02:32:38 +0700

    use case work with infrastructure

--- src/application/use_cases/EmotionalUseCase.py
+++ src/application/use_cases/EmotionalUseCase.py
@@ -1,6 +1,6 @@
 import asyncio
 from typing import List
-from src.core.entities import EmotionalCoefficient
+from src.core.entities.EmotionalCoefficient import EmotionalCoefficient
 from src.core.interfaces.IEmotionalClassification import IEmotionalClassification
 from src.infrastructure.emotion_classification.EmotionClassification import EmotionalClassification
 
@@ -14,10 +14,21 @@ class EmotionalUseCase:
             'вина', 'стыд'
         ]
 
-    async def analyze_single_text(self, text: str) -> EmotionalCoefficient:
-        emotions = await self.emotional_classification.extract_emotion(text)
+    async def analyze_single_message(self, message: str) -> EmotionalCoefficient:
+        emotions = await self.emotional_classification.extract_emotion(message)
 
-        emotion_dict = {field: 0.0 for field in EmotionalCoefficient.__dataclass_fields__}
+        emotion_dict = {
+            "neutral": 0.0,
+            "joy": 0.0,
+            "sadness": 0.0,
+            "anger": 0.0,
+            "enthusiasm": 0.0,
+            "surprise": 0.0,
+            "disgust": 0.0,
+            "fear": 0.0,
+            "guilt": 0.0,
+            "shame": 0.0,
+        }
 
         mapping = {
             'нейтрально': 'neutral',
@@ -35,12 +46,19 @@ class EmotionalUseCase:
         for emotion_name, score in emotions:
             if emotion_name in mapping:
                 emotion_dict[mapping[emotion_name]] = score
+
         return EmotionalCoefficient(**emotion_dict)
+
+    async def analyze_messages_batch(self, messages: List[str]) -> List[EmotionalCoefficient]:
+        """Асинхронно обрабатывает массив текстов"""
+        tasks = [self.analyze_single_message(text) for text in messages]
+        return await asyncio.gather(*tasks)
 
 
 async def main():
     use_case = EmotionalUseCase(emotional_classification=EmotionalClassification())
-    await use_case.emotional_classification.extract_emotion("Я люблю мэдкида")
+    list = await use_case.analyze_messages_batch(["Я люблю мэдкида", 'я ненавижу мэдкида'])
+    print()
 
 if __name__ == "__main__":
     asyncio.run(main())
